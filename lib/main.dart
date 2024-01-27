c80beaa977f30e26ee592744eb18a20bf54d1154
victor jose <victorjosegonzalez9@gmail.com>
2024-01-27 15:16:26 -0400

    ua

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:paralelos2/product_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
